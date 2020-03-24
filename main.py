d7f51d8986e44b68eb50465e70f03ec9aa2d7e56
Andrew M. Silva <andrewsaxx@gmail.com>
2020-03-24 12:32:54 -0300

    Improving steps dfinition

--- main.py
+++ main.py
@@ -13,11 +13,14 @@ np.random.seed(59)
 DB_NAME = 'all-the-news.db'
 PROCESSED_CSV = 'processed.csv'
 DICTIONARY = 'dictionary.bin'
-BOW_CORPUS = 'bag-of-words.bin'
 TFIDF_CORPUS = 'tf-idf.bin'
 LDA_MODEL = 'lda-model.bin'
 
+def printExecutionTime(start_time):
+  print('Completed in {0:.4f} seconds\n'.format(time.time() - start_time))
+
 def loadData():
+  print('Loading data...')
   start_time = time.time()
 
   try:
@@ -25,66 +28,66 @@ def loadData():
     is_processed = True
   except:
     conn = sqlite3.connect(DB_NAME)
-    df = pd.read_sql_query('SELECT title, content FROM longform WHERE content IS NOT NULL', conn)
+    df = pd.read_sql_query('SELECT author, date, year, publication, content FROM longform WHERE content != "" AND content IS NOT NULL', conn)
     conn.commit()
     conn.close()
     is_processed = False
   
-  print('Data loading: {0:.4f} seconds'.format(time.time() - start_time))
-  
+  printExecutionTime(start_time)
   return df, is_processed
 
 def preProcess(df, column, is_processed=False):
+  print('Pre-processing...')
   start_time = time.time()
 
-  if not is_processed:
+  processed_docs = None
+  if is_processed:
+    processed_docs = [ doc.split() for doc in df[column] ]
+  else:
     stop_words = gensim.parsing.preprocessing.STOPWORDS
     simple_preprocess = gensim.utils.simple_preprocess
     stemmer = nltk.SnowballStemmer('english')
     lemmatizer = nltk.WordNetLemmatizer()
 
-    processed_texts = []
-    for text in df[column]:
-      processed_text = []
-      for token in simple_preprocess(text):
-        if token not in stop_words and len(token) > 3:
+    processed_docs = []
+    for doc in df[column]:
+      processed_doc = []
+      for token in simple_preprocess(doc):
+        if token not in stop_words:
           token = lemmatizer.lemmatize(token, pos='v')
           token = stemmer.stem(token)
-          processed_text.append(token)
-      processed_texts.append(' '.join(processed_text))
+          processed_doc.append(token)
+      processed_docs.append(processed_doc)
     
-    df[column] = processed_texts
+    df[column] = [ ' '.join(doc) for doc in processed_docs ]
     df[column].to_csv(PROCESSED_CSV)
 
-  print('Pre-processing: {0:.4f} seconds'.format(time.time() - start_time))
+  printExecutionTime(start_time)
+  return processed_docs
 
-def extractFeatures(df, column):
+def extractDictionary(documents):
+  print('Extracting dictionary...')
   start_time = time.time()
-
-  texts = [ text.split() for text in df[column] if type(text) is str ]
 
   try:
     dictionary = gensim.corpora.Dictionary.load(DICTIONARY)
   except:
-    dictionary = gensim.corpora.Dictionary(texts)
-    dictionary.filter_extremes(no_below=15, no_above=0.5, keep_n=100000)
+    dictionary = gensim.corpora.Dictionary(documents)
+    dictionary.filter_extremes(no_below=200, no_above=0.8, keep_n=1000)
     dictionary.save(DICTIONARY)
 
-  try:
-    f = open(BOW_CORPUS, 'rb')
-    bow_corpus = pickle.load(f)
-  except:
-    bow_corpus = [dictionary.doc2bow(doc) for doc in texts]
+  printExecutionTime(start_time)
+  return dictionary
 
-    f = open(BOW_CORPUS, 'wb')
-    pickle.dump(bow_corpus, f)
-  finally:
-    f.close()
+def extractFeatures(documents, dictionary):
+  print('Extracting features...')
+  start_time = time.time()
   
   try:
     f = open(TFIDF_CORPUS, 'rb')
     tfidf_corpus = pickle.load(f)
   except:
+    bow_corpus = [ dictionary.doc2bow(doc) for doc in documents ]
     tfidf = gensim.models.TfidfModel(bow_corpus)
     tfidf_corpus = tfidf[bow_corpus]
 
@@ -93,11 +96,11 @@ def extractFeatures(df, column):
   finally:
     f.close()
 
-  print('Feature extraction: {0:.4f} seconds'.format(time.time() - start_time))
-
-  return dictionary, bow_corpus, tfidf_corpus
+  printExecutionTime(start_time)
+  return tfidf_corpus
 
 def generateLDA(corpus, dictionary):
+  print('Generating LDA...')
   start_time = time.time()
 
   try:
@@ -111,13 +114,17 @@ def generateLDA(corpus, dictionary):
     )
     model.save(LDA_MODEL)
   
-  print('LDA generating: {0:.4f} seconds'.format(time.time() - start_time))
-  
+  printExecutionTime(start_time)
   return model
 
+collection = 'content'
+author = 'publication'
+
 news_df, is_processed = loadData()
-preProcess(news_df, 'content', is_processed)
-dictionary, bow_corpus, tfidf_corpus = extractFeatures(news_df, 'content')
+processed = preProcess(news_df, collection, is_processed)
+
+dictionary = extractDictionary(processed)
+tfidf_corpus = extractFeatures(processed, dictionary)
 
 lda_model = generateLDA(tfidf_corpus, dictionary)
 for idx, topic in lda_model.print_topics(-1):
